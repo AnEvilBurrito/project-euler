07fb9bbbba3ff0971ffa25a137fc218d385be5a1
AnEvilBurrito <47137689+AnEvilBurrito@users.noreply.github.com>
2021-11-27 16:36:34 +1100

    supdate

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 # File naming convention 
 
 They are always named "problem-n-x", where n is the problem number, and x is a method to solve it.  
+
+Different methods may have different time and space complexities. 
+
+All code should be executable by running the .py file using Python 3.8.3 
--- problem-1.py
+++ problem-1.py
@@ -1,3 +1,24 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
+
+x = []
+
+i = 1
+while i < 1000:
+    
+    # x only takes in natural numbers that is fully divisible by 5 or 3 
+
+    if i % 5 == 0:
+        x.append(i)
+
+    elif i % 3 == 0: 
+        x.append(i)
+
+    i += 1  
+
+x = list(set(x)) # removing duplicates in the list x 
+print(sum(x))
+
+# time complexity: O(N), N = amount of natural numbers iterated, e.g. 1000
+# space complexity: O(N/3 + N/5) == O(N), assuming if every member can be divisible by 3 or 5, then 1/3 + 1/5 of digits will be retained 
